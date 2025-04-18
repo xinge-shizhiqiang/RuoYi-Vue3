1258815a9e1b7f707adafe2f5ea14a00cb20436a
shizhiqiang <912034159@qq.com>
2025-04-18 17:19:35 +0800

    新登录页部分优化

--- src/views/loginv2.vue
+++ src/views/loginv2.vue
@@ -1,67 +1,37 @@
 <template>
   <div class="login">
     <div>
-      <el-form
-        ref="loginRef"
-        :model="loginForm"
-        :rules="loginRules"
-        class = "login-form"
-        label-width="70px"
-      >
-      <h3 class="title">{{ title }}</h3>
+      <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form" label-width="70px">
+        <h3 class="title">{{ title }}</h3>
         <el-form-item prop="username" label="用户名">
-          <el-input
-            v-model="loginForm.username"
-            type="text"
-            size="large"
-            auto-complete="off"
-            placeholder="用户名"
-          >
-          <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
+          <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="用户名">
+            <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
           </el-input>
         </el-form-item>
         <el-form-item prop="password" label="密码">
-          <el-input
-            v-model="loginForm.password"
-            type="password"
-            size="large"
-            auto-complete="off"
-            placeholder="密码"
-            @keyup.enter = "handleLogin"
-          >
-          <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
+          <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" placeholder="密码"
+            @keyup.enter="handleLogin">
+            <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
           </el-input>
         </el-form-item>
         <el-form-item prop="code" label="验证码" v-if="captchaEnabled">
-            <el-input
-              v-model="loginForm.code"
-              auto-complete="off"
-              placeholder="验证码"
-              style="width: 63%"
-              @keyup.enter="handleLogin"
-            >
+          <el-input v-model="loginForm.code" auto-complete="off" placeholder="验证码" style="width: 50%"
+            @keyup.enter="handleLogin">
             <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
-            </el-input>
-            <div class="login-code">
-              <img :src="codeUrl" @click="getCode" class="login-code-img"/>
-            </div>
+          </el-input>
+          <div class="login-code">
+            <img :src="codeUrl" @click="getCode" class="login-code-img" />
+          </div>
         </el-form-item>
-        <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-        <el-form-item>
-            <el-button 
-              :loading="loading" 
-              size="large"
-              type="primary"
-              style="width:100%;"
-              @click.prevent="handleLogin"
-            >
-              <span v-if="!loading">登 录</span>
-              <span v-else>登 录 中...</span>
-            </el-button>
-            <div style="float: right;" v-if="register">
-              <router-link class="link-type" :to="'/register'">立即注册</router-link>
-            </div>
-        </el-form-item>
+        <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 15px 15px;">记住密码</el-checkbox>
+        <el-button :loading="loading" size="large" type="primary" style="width:100%;margin-bottom: 10px;"
+          @click.prevent="handleLogin">
+          <span v-if="!loading">登 录</span>
+          <span v-else>登 录 中...</span>
+        </el-button>
+        <div style="text-align: right;margin-bottom:10px;">
+          <router-link class="link-type" :to="'/register'">立即注册</router-link>
+        </div>
       </el-form>
     </div>
     <div class="el-login-footer">
@@ -101,11 +71,11 @@ const loading = ref(false);
 // 验证码开关
 const captchaEnabled = ref(true);
 // 注册开关
-const register = ref(false);
+const register = ref(true);
 const redirect = ref(undefined);
 
 watch(route, (newRoute) => {
-    redirect.value = newRoute.query && newRoute.query.redirect;
+  redirect.value = newRoute.query && newRoute.query.redirect;
 }, { immediate: true });
 
 function handleLogin() {
@@ -175,43 +145,51 @@ getCookie();
   background-image: url("../assets/images/20250330.jpg");
   background-size: cover;
 }
+
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
-  color: #707070;
+  color: #67C23A;
 }
 
 .login-form {
   border-radius: 6px;
-  background: #ffffff;
+  background: #fff;
   width: 400px;
   padding: 25px 25px 5px 25px;
+
   .el-input {
     height: 40px;
+
     input {
       height: 40px;
     }
   }
+
   .input-icon {
     height: 39px;
     width: 14px;
     margin-left: 0px;
   }
 }
+
 .login-tip {
   font-size: 13px;
   text-align: center;
   color: #bfbfbf;
 }
+
 .login-code {
   width: 33%;
   height: 40px;
   float: right;
+
   img {
     cursor: pointer;
     vertical-align: middle;
   }
 }
+
 .el-login-footer {
   height: 40px;
   line-height: 40px;
@@ -224,6 +202,7 @@ getCookie();
   font-size: 12px;
   letter-spacing: 1px;
 }
+
 .login-code-img {
   height: 40px;
   padding-left: 12px;
